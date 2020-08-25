4d90e68bca6aa3ab40a834fc2a4f881f3fd2f58f
Michael Tidwell <mtidwell021@gmail.com>
2020-08-25 18:17:09 -0400

    update cw for internal alerts back to enable

--- cw.tf
+++ cw.tf
@@ -13,7 +13,6 @@ module "kong_external_lb_cw" {
 module "kong_internal_lb_cw" {
   source = "./cw/lb"
 
-  enable        = var.enable_external_lb
   load_balancer = coalesce(join("", aws_lb.internal.*.arn_suffix), "none")
   target_group  = coalesce(join("", aws_lb_target_group.internal.*.arn), "none")
 
