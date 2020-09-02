2342e07e986d9bda13691c98a39be0b1ef2cff52
Michael Tidwell <mtidwell021@gmail.com>
2020-09-01 22:09:58 -0400

    updating alarm descriptions

--- cw/lb/main.tf
+++ cw/lb/main.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_cloudwatch_metric_alarm" "unhealthy-host-count" {
   count = var.enable ? 1 : 0
 
-  alarm_name          = format("%s-unhealthy-host-count", local.load_balancer)
+  alarm_name          = format("KONG01-%s-unhealthy-host-count", local.load_balancer)
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "UnHealthyHostCount"
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_metric_alarm" "unhealthy-host-count" {
 
   actions_enabled   = "true"
   alarm_actions     = var.cloudwatch_actions
-  alarm_description = format("Unhealthy host count is greater than 0 for %s", local.load_balancer)
+  alarm_description = format("KONG01 Unhealthy host count is greater than 0 for %s", local.load_balancer)
   ok_actions        = var.cloudwatch_actions
 
   dimensions = {
@@ -28,7 +28,7 @@ resource "aws_cloudwatch_metric_alarm" "unhealthy-host-count" {
 resource "aws_cloudwatch_metric_alarm" "http-code-4xx-count" {
   count = var.enable ? 1 : 0
 
-  alarm_name          = format("%s-http-code-4xx-count", local.load_balancer)
+  alarm_name          = format("KONG-M-%s-http-code-4xx-count", local.load_balancer)
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HTTPCode_Target_4XX_Count"
@@ -51,7 +51,7 @@ resource "aws_cloudwatch_metric_alarm" "http-code-4xx-count" {
 resource "aws_cloudwatch_metric_alarm" "http-code-5xx-count" {
   count = var.enable ? 1 : 0
 
-  alarm_name          = format("%s-http-code-5xx-count", local.load_balancer)
+  alarm_name          = format("KONG-M-%s-http-code-5xx-count", local.load_balancer)
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HTTPCode_Target_5XX_Count"
